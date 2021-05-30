f7e65455ba9255b0256fcde7dfaa22be61640eb6
Jin <gimquokka@gmail.com>
2021-05-30 17:21:50 +0900

    init project

---
+++ src/main/java/com/inflearn/intro_rest_api/SpringIntroRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.intro_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inflearn/intro_rest_api/SpringIntroRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.intro_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
